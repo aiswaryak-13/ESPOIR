7abcd4ca5abc7a1466c5975e4182addfb50453ee
Aiswarya K <your-email@example.com>
2025-07-19 09:19:02 +0530

    Track all project files

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 const express = require('express');
 const app = express();
-const env = require('dotenv').config();
-const db  = require('./config/db');
+const path = require('path');
+const session = require('express-session');
+const dotenv = require('dotenv');
+dotenv.config();
 
+const passport = require('./config/passport')
+const db  = require('./config/db');
+const userRouter = require('./routes/userRouter');
 db();
- 
-app.listen(process.env.PORT,()=>{
-  console.log(`Server running at http://localhost:${process.env.PORT}`);
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(session({
+  secret:process.env.SESSION_SECRET,
+  resave:false,
+  saveUninitialized:true,
+  cookie:{
+    secure:false,
+    httpOnly:true,
+    maxAge:72*60*60*1000
+  }
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req,res,next)=>{
+  res.set('Cache-control','no-store');
+  next();
+})
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/',userRouter);
+
+const PORT =process.env.PORT ||  3111 ;
+app.listen(PORT,()=>{
+  console.log(`Server running at http://localhost:${PORT}`);
 });
